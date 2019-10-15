f4e1f4237a695ab8c111970c7df031c8c6c13e56
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-10-15 09:54:06 +0530

    Added cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,11 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+
+firestore.settings({ timestampsInSnapshots: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +13,26 @@ const functions = require("firebase-functions");
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+exports.getAllPost = functions.https.onRequest(async (req, res) => {
+  const snapshot = await firestore.collection("posts").get();
+  const posts = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  res.json({ posts });
+});
+
+exports.sanitizeContent = functions.firestore
+  .document("post/{postId}")
+  .onWrite(async change => {
+    // Delete
+    if (!change.after.exists) return;
+
+    const { content, sanitized } = change.after.data();
+
+    if (content && !sanitized) {
+      return change.after.ref.update({
+        content: content.replace(/CoffeeScript/g, "*********"),
+        sanitized: true
+      });
+    }
+    return null;
+  });
